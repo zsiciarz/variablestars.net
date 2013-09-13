90a13bc965e4178c021e36ef52fec873a3934b29
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-13 17:36:42 +0200

    More fields in star admin.

--- stars/admin.py
+++ stars/admin.py
@@ -5,4 +5,12 @@ from django.contrib import admin
 from .models import Star
 
 
-admin.site.register(Star)
+class StarAdmin(admin.ModelAdmin):
+    list_display = (
+        'get_constellation_display', 'name', 'variable_type',
+        'min_magnitude', 'max_magnitude',
+    )
+    list_display_links = ('name',)
+
+
+admin.site.register(Star, StarAdmin)
