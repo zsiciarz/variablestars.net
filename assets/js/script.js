d7ef3ee7b71fad5fe52ac6efe49f228576b957f4
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-13 21:41:49 +0200

    Added initial secondary navbar.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 (function ($) {
 
     $(document).ready(function () {
-        // TODO
+        /**
+         * Updates JD value displayed on the secondary navbar.
+         */
+        var updateJulianDate = function () {
+            var jd = new Date().getTime() / 86400000 + 2440587.5;
+            $("#current-jd").text(jd.toFixed(4));
+            setTimeout(updateJulianDate, 1000);
+        };
+        updateJulianDate();
     });
 
 })(jQuery);
