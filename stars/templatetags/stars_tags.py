325e2d87d994a563744825c774fcbf5c3f9d7172
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-14 19:15:19 +0200

    Added template tag to gray out magnitudes over limit.

--- stars/templatetags/stars_tags.py
+++ stars/templatetags/stars_tags.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django import template
+from django.utils.safestring import mark_safe
 
 import ephem
 
@@ -17,3 +18,17 @@ def round_degrees(angle):
     :param angle: an instance of ephem.Angle class
     """
     return int(round(angle / ephem.degree))
+
+
+@register.simple_tag(takes_context=True)
+def magnitude(context, value):
+    """
+    Grays out magnitude value if it is below current observer's limit.
+    """
+    observer = context.get('current_observer')
+    if not observer:
+        return value
+    if value > observer.limiting_magnitude:
+        return mark_safe('<span class="text-muted">%s</span>' % value)
+    else:
+        return value
