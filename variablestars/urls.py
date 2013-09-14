55d358e99a2be4b365d1226a71365851c996e0a1
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-14 15:10:10 +0200

    Added logout link.

--- variablestars/urls.py
+++ variablestars/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'variablestars.views.index', name='main'),
+    url(r'', include('django.contrib.auth.urls')),
     url(r'^stars/', include('stars.urls', namespace='stars')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^404/', TemplateView.as_view(template_name='404.html')),
