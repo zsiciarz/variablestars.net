497d6b8d76eb3bcdcc871828a54885859ff3da95
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-14 17:07:30 +0200

    Display thank you information after registering.

--- observers/models.py
+++ observers/models.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from model_utils.models import TimeStampedModel
+from registration.signals import user_registered
 
 
 @python_2_unicode_compatible
@@ -40,4 +42,14 @@ def create_observer(sender, instance, created, **kwargs):
 post_save.connect(
     create_observer, sender=User,
     dispatch_uid='observers.models.create_observer'
+)
+
+
+def complete_registration(sender, user, request, **kwargs):
+    messages.success(request, _("Thank you for your registration!"))
+
+
+user_registered.connect(
+    complete_registration,
+    dispatch_uid='observers.models.complete_registration'
 )
