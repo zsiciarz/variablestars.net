ff6eaa806d24f04df6ce834a760cc5ee7dea9fdd
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-14 22:39:23 +0200

    Redirect from home to observer's profile.

--- variablestars/views.py
+++ variablestars/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
+    if request.user.is_authenticated():
+        return redirect(request.user)
     return render(request, "index.html", {})
