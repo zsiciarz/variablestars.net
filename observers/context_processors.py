ba4764ab003ad60f860f664b10fdc8fad35e68e7
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-14 18:34:12 +0200

    That was silly.

--- observers/context_processors.py
+++ observers/context_processors.py
@@ -5,7 +5,7 @@ from .models import Observer
 
 
 def get_current_observer(request):
-    if request.user and request.user.is_authenticated:
+    if request.user and request.user.is_authenticated():
         current_observer = Observer.objects.get(user=request.user)
     else:
         current_observer = None
