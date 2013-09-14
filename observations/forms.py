1632b64372f2f38a6c43b000ace631d183278375
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-15 00:27:13 +0200

    Use transaction.atomic in batch uploader.

--- observations/forms.py
+++ observations/forms.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.db import transaction
 from django.utils.translation import ugettext_lazy as _
 
 from pyaavso.formats.visual import VisualFormatReader
@@ -18,33 +19,34 @@ class BatchUploadForm(forms.Form):
         fp = self.cleaned_data['aavso_file']
         reader = VisualFormatReader(fp)
         observer = Observer.objects.get(aavso_code=reader.observer_code)
-        for row in reader:
-            try:
-                star = Star.objects.get(name=row['name'])
-                fainter_than = '<' in row['magnitude']
-                magnitude = float(row['magnitude'].replace('<', ''))
-                jd = float(row['date'])
+        with transaction.atomic():
+            for row in reader:
                 try:
-                    observation = Observation.objects.get(
-                        observer=observer,
-                        star=star,
-                        jd=jd,
-                    )
-                except Observation.DoesNotExist:
-                    observation = Observation(
-                        observer=observer,
-                        star=star,
-                        jd=jd,
-                    )
-                observation.magnitude = magnitude
-                observation.fainter_than = fainter_than
-                observation.comp1 = row['comp1']
-                observation.comp2 = row['comp2']
-                observation.chart = row['chart']
-                observation.comment_code = row['comment_code']
-                observation.notes = row['notes']
-                observation.save()
-            except Exception as e:
-                print row
-                print e
-                continue
+                    star = Star.objects.get(name=row['name'])
+                    fainter_than = '<' in row['magnitude']
+                    magnitude = float(row['magnitude'].replace('<', ''))
+                    jd = float(row['date'])
+                    try:
+                        observation = Observation.objects.get(
+                            observer=observer,
+                            star=star,
+                            jd=jd,
+                        )
+                    except Observation.DoesNotExist:
+                        observation = Observation(
+                            observer=observer,
+                            star=star,
+                            jd=jd,
+                        )
+                    observation.magnitude = magnitude
+                    observation.fainter_than = fainter_than
+                    observation.comp1 = row['comp1']
+                    observation.comp2 = row['comp2']
+                    observation.chart = row['chart']
+                    observation.comment_code = row['comment_code']
+                    observation.notes = row['notes']
+                    observation.save()
+                except Exception as e:
+                    print row
+                    print e
+                    continue
