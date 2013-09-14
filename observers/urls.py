209f1bdd4c52cb25001b29c9c2b76cbdc274c135
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-14 17:54:18 +0200

    Added ObserverListView.

--- observers/urls.py
+++ observers/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import ObserverDetailView
+from .views import ObserverListView, ObserverDetailView
 
 
 urlpatterns = patterns('',
+    url(r'^$', ObserverListView.as_view(), name='observer_list'),
     url(r'^(?P<pk>\d+)/$', ObserverDetailView.as_view(), name='observer_detail'),
 )
--- observers/views.py
+++ observers/views.py
@@ -5,6 +5,20 @@ from django.views.generic import ListView, DetailView
 from .models import Observer
 
 
+class ObserverListView(ListView):
+    """
+    Display a list of observers.
+    """
+    model = Observer
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        context = super(ObserverListView, self).get_context_data(**kwargs)
+        queryset = self.get_queryset()
+        context['total_observer_count'] = queryset.count()
+        return context
+
+
 class ObserverDetailView(DetailView):
     """
     Public profile of an observer.
