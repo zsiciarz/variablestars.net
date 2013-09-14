76ef20a926f0ba37876ddf3e537422bbc79c4539
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-14 19:50:12 +0200

    Customized password reset templates.

--- observers/urls.py
+++ observers/urls.py
@@ -7,5 +7,7 @@ urlpatterns = patterns('',
     url(r'^$', ObserverListView.as_view(), name='observer_list'),
     url(r'^(?P<pk>\d+)/$', ObserverDetailView.as_view(), name='observer_detail'),
     url(r'^edit/$', ObserverEditView.as_view(), name='observer_edit'),
-    url(r'^change_password/$', 'django.contrib.auth.views.password_change', name='change_password', kwargs={'post_change_redirect': '/'})
+    url(r'^change_password/$', 'django.contrib.auth.views.password_change', name='change_password', kwargs={'post_change_redirect': '/'}),
+    url(r'^reset_password/$', 'django.contrib.auth.views.password_reset', name='reset_password', kwargs={'post_reset_redirect': 'auth_password_reset_done'}),
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name='reset_password_confirm', kwargs={'post_reset_redirect': 'auth_password_reset_complete'}),
 )
