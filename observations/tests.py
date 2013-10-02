d74b050f1f70a65d2fd1d39f8b3f24eb802d4a6e
Zbigniew Siciarz <antyqjon@gmail.com>
2013-10-02 21:27:16 +0200

    Test for Observation.__str__

--- observations/tests.py
+++ observations/tests.py
@@ -37,6 +37,21 @@ class ObservationModelTestCase(TestCase):
             min_magnitude=11.3,
         )
 
+    def test_str(self):
+        """
+        Check for string representation of an observation.
+        """
+        observation = Observation(
+            observer=self.observer,
+            star=self.star,
+            jd=2456567.2550,
+            magnitude=8.5,
+        )
+        expected = "%s %s %s" % (
+            observation.star, observation.jd, observation.magnitude,
+        )
+        self.assertEqual(str(observation), expected)
+
     def test_update_star_observations_count(self):
         """
         Check that creating new observations updates star's observations_count
