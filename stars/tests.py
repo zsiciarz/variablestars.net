66a9b56da94973f5ac2ed303d5d5468d060f993d
Zbigniew Siciarz <antyqjon@gmail.com>
2013-10-03 20:03:35 +0200

    More tests for periodicity.

--- stars/tests.py
+++ stars/tests.py
@@ -17,4 +17,25 @@ class StarModelTestCase(BaseTestCase):
         self.assertEqual(str(self.star), self.star.name)
 
     def test_is_not_periodic(self):
+        """
+        Star without period or epoch is not periodic.
+        """
+        self.assertIsNone(self.star.period)
+        self.assertIsNone(self.star.epoch)
         self.assertFalse(self.star.is_periodic())
+
+    def test_is_not_periodic_only_period(self):
+        """
+        Period alone is not enough to consider the star periodic.
+        """
+        self.star.period = 309.95
+        self.assertIsNone(self.star.epoch)
+        self.assertFalse(self.star.is_periodic())
+
+    def test_is_periodic(self):
+        """
+        Star is periodic only when it has a defined period and epoch.
+        """
+        self.assertIsNotNone(self.periodic_star.period)
+        self.assertIsNotNone(self.periodic_star.epoch)
+        self.assertTrue(self.periodic_star.is_periodic())
