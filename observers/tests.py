d54556178c1fed9caa134d5bc786370e670a5402
Zbigniew Siciarz <antyqjon@gmail.com>
2013-10-04 21:05:25 +0200

    Added initial tests for Observer model.

--- observers/tests.py
+++ observers/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+from __future__ import unicode_literals
+
+from variablestars.tests import BaseTestCase
+
+
+class ObserverModelTestCase(BaseTestCase):
+    """
+    Tests for Observer model.
+    """
+
+    def test_str(self):
+        """
+        String representation of an observer is his username (sometimes with
+        full name).
+        """
+        self.assertEqual(str(self.observer), self.user.username)
+        self.user.first_name = 'John'
+        self.user.last_name = 'Doe'
+        self.assertEqual(str(self.observer), 'stargazer (John Doe)')
