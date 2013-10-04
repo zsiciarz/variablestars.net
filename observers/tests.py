e4c4657be77d10222e2ecba2d45f3ce062f84b98
Zbigniew Siciarz <antyqjon@gmail.com>
2013-10-04 21:23:44 +0200

    A few more tests for Observers.

--- observers/tests.py
+++ observers/tests.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals
 
+from observations.models import Observation
 from variablestars.tests import BaseTestCase
 
 
@@ -9,6 +10,23 @@ class ObserverModelTestCase(BaseTestCase):
     """
     Tests for Observer model.
     """
+
+    def setUp(self):
+        super(ObserverModelTestCase, self).setUp()
+        for i in range(10):
+            Observation.objects.create(
+                observer=self.observer,
+                star=self.star,
+                jd=2456567.2550 + i,
+                magnitude=8.5 + 0.1 * i,
+            )
+        for i in range(5):
+            Observation.objects.create(
+                observer=self.observer,
+                star=self.periodic_star,
+                jd=2456567.2550 + i,
+                magnitude=6.5 - 0.2 * i,
+            )
 
     def test_str(self):
         """
@@ -19,3 +37,14 @@ class ObserverModelTestCase(BaseTestCase):
         self.user.first_name = 'John'
         self.user.last_name = 'Doe'
         self.assertEqual(str(self.observer), 'stargazer (John Doe)')
+
+    def test_top_stars(self):
+        expected = [
+            {'star_id': self.star.id, 'star__name': self.star.name, 'observations_count': 10},
+            {'star_id': self.periodic_star.id, 'star__name': self.periodic_star.name, 'observations_count': 5},
+        ]
+        top_stars = list(self.observer.top_stars())
+        self.assertEqual(top_stars, expected)
+
+    def test_observed_star_count(self):
+        self.assertEqual(self.observer.observed_stars_count(), 2)
