75f766b7b61ad90fdd44d5e00355a14960249381
Zbigniew Siciarz <antyqjon@gmail.com>
2013-10-11 19:28:34 +0200

    Added total stats to homepage.

--- variablestars/views.py
+++ variablestars/views.py
@@ -2,8 +2,16 @@
 
 from django.shortcuts import render, redirect
 
+from stars.models import Star
+from observations.models import Observation
+from observers.models import Observer
+
 
 def index(request):
     if request.user.is_authenticated():
         return redirect(request.user)
-    return render(request, "index.html", {})
+    return render(request, "index.html", {
+        'stars_count': Star.objects.count(),
+        'observers_count': Observer.objects.count(),
+        'observations_count': Observation.objects.count(),
+    })
