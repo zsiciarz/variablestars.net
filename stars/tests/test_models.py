e8c1a4753bcf831b1e5b9b2e3d370daeb6f05d54
Zbigniew Siciarz <antyqjon@gmail.com>
2013-12-31 23:59:17 +0100

    Unit tests are unit.

--- stars/tests/test_models.py
+++ stars/tests/test_models.py
@@ -32,45 +32,40 @@ class StarModelBasicPropertiesTestCase(TestDataMixin, unittest.TestCase):
     """
     Tests for Star model - basic properties.
     """
-    def setUp(self):
-        super(StarModelBasicPropertiesTestCase, self).setUp()
-        self._create_stars(save=False)
-
     def test_str(self):
         """
         String representation of a star is just its name.
         """
-        self.assertEqual(str(self.star), self.star.name)
+        star = Star(name='R LEO')
+        self.assertEqual(str(star), star.name)
 
     def test_is_not_periodic(self):
         """
         Star without period or epoch is not periodic.
         """
-        self.assertIsNone(self.star.period)
-        self.assertIsNone(self.star.epoch)
-        self.assertFalse(self.star.is_periodic())
+        star = Star(name='R LEO', period=None, epoch=None)
+        self.assertFalse(star.is_periodic())
 
     def test_is_not_periodic_only_period(self):
         """
         Period alone is not enough to consider the star periodic.
         """
-        self.star.period = 309.95
-        self.assertIsNone(self.star.epoch)
-        self.assertFalse(self.star.is_periodic())
+        star = Star(name='R LEO', period=309.95, epoch=None)
+        self.assertFalse(star.is_periodic())
 
     def test_is_periodic(self):
         """
         Star is periodic only when it has a defined period and epoch.
         """
-        self.assertIsNotNone(self.periodic_star.period)
-        self.assertIsNotNone(self.periodic_star.epoch)
-        self.assertTrue(self.periodic_star.is_periodic())
+        star = Star(name='T CEP', period=388.14, epoch=2444177.0)
+        self.assertTrue(star.is_periodic())
 
     def test_gcvs_name(self):
         """
         Check that space is replaced by + in GCVS search name.
         """
-        self.assertEqual(self.star.get_gcvs_search_name(), 'R+LEO')
+        star = Star(name='R LEO')
+        self.assertEqual(star.get_gcvs_search_name(), 'R+LEO')
 
 
 class StarObservationsModelTestCase(BaseTestCase):
