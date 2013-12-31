3d9cc6e5c0c107fb99965fcd3a065c602cf4c8bc
Zbigniew Siciarz <antyqjon@gmail.com>
2014-01-01 00:56:11 +0100

    Don't hit the DB while testing ObserverMiddleware.

--- observers/tests/test_views.py
+++ observers/tests/test_views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AnonymousUser, User
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
-from mock import MagicMock
+from mock import MagicMock, patch
 
 from ..middleware import ObserverMiddleware
 from ..models import Observer
@@ -20,16 +20,24 @@ class ObserverMiddlewareTestCase(BaseTestCase):
     def setUp(self):
         super(ObserverMiddlewareTestCase, self).setUp()
         self.request = MagicMock()
-        self.request.user = self.user
+        self.user = self.request.user = User(
+            username='stargazer',
+            email='stargazer@example.com',
+            password='123456',
+        )
 
     def test_authenticated_user(self):
         """
         Check that the middleware attaches an Observer instance to the request
         for authenticated users.
         """
+        observer = Observer(user=self.user, aavso_code='XYZ')
         middleware = ObserverMiddleware()
-        middleware.process_request(self.request)
-        self.assertEqual(self.request.observer, self.observer)
+        with patch.object(Observer.objects, 'get') as mock_get:
+            mock_get.return_value = observer
+            middleware.process_request(self.request)
+            mock_get.assert_called_once_with(user=self.user)
+        self.assertEqual(self.request.observer, observer)
 
     def test_anonymous_user(self):
         """
