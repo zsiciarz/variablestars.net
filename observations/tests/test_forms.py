ec0b6feaf3b55e245c386890f869590565698afc
Zbigniew Siciarz <antyqjon@gmail.com>
2014-01-01 01:23:39 +0100

    Use basic TestCase for BatchUploadForm tests.

--- observations/tests/test_forms.py
+++ observations/tests/test_forms.py
@@ -2,21 +2,24 @@
 
 from __future__ import unicode_literals
 
+import unittest
+
 from mock import patch
 
 from ..forms import BatchUploadForm
 from ..utils import jd_now
+from observers.models import Observer
 from stars.models import Star
-from variablestars.tests.base import BaseTestCase
 
 
-class BatchUploadFormTestCase(BaseTestCase):
+class BatchUploadFormTestCase(unittest.TestCase):
     """
     Tests for ``observations.forms.BatchUploadForm`` class.
     """
     def setUp(self):
         super(BatchUploadFormTestCase, self).setUp()
         self.star = Star(name='R LEO')
+        self.observer = Observer(aavso_code='XYZ')
         self.row = {
             'name': self.star.name,
             'magnitude': '6.6',
@@ -45,5 +48,6 @@ class BatchUploadFormTestCase(BaseTestCase):
         with patch.object(Star.objects, 'get') as mock_get:
             mock_get.return_value = self.star
             observation = form.process_row(self.row, self.observer)
+        self.assertEqual(observation.observer, self.observer)
         self.assertEqual(observation.star, self.star)
         self.assertEqual(observation.notes, 'test2')
