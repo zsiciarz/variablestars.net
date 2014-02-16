fea66cc0d52c5973c78aaf2bb7e45d4376227953
Zbigniew Siciarz <antyqjon@gmail.com>
2014-02-16 14:18:04 +0100

    Narrowed down the scope of warning filter.

--- stars/tests/test_views.py
+++ stars/tests/test_views.py
@@ -123,12 +123,12 @@ class StarDetailViewTestCase(BaseTestCase):
         self.assertIsNotNone(response.context['next_rising'])
 
     def test_circumpolar_star(self):
+        self._create_stars()
+        self.star.dec = '+89:00:00'
+        self.star.save()
+        url = self.star.get_absolute_url()
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore', category=DeprecationWarning)
-            self._create_stars()
-            self.star.dec = '+89:00:00'
-            self.star.save()
-            url = self.star.get_absolute_url()
             response = self.client.get(url)
             self.assertIsNone(response.context['next_rising'])
 
