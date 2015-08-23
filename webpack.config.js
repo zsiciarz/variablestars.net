71ff2abd502b65cf1338cfb9ba4c36ad0fda00c7
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-08-23 16:05:20 +0200

    Use webpack to build CSS from LESS.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry: {
         app: './assets/js/script.coffee',
         lightcurve: './assets/js/lightcurve.coffee',
         add_observation: './assets/js/add_observation.coffee',
-        registration_form: './assets/js/registration_form.coffee'
+        registration_form: './assets/js/registration_form.coffee',
+        style: './assets/less/style.less'
     },
     output: {
         path: './assets/build',
@@ -14,6 +16,12 @@ module.exports = {
             test: /\.coffee$/,
             exclude: /node_modules/,
             loader: 'coffee'
+        }, {
+            test: /\.less$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
         }]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("style.css")
+    ]
 };
