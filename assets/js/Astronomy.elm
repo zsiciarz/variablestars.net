47ea48af7d120cc5d49a36ddae0607a8087be12f
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-10-03 23:37:01 +0200

    Add complementary jdToTime function.

--- assets/js/Astronomy.elm
+++ assets/js/Astronomy.elm
@@ -1,4 +1,4 @@
-module Astronomy (JD, timeToJd) where
+module Astronomy (JD, timeToJd, jdToTime) where
 
 import Time exposing (Time)
 
@@ -9,3 +9,8 @@ type alias JD = Float
 timeToJd : Time -> JD
 timeToJd t =
     t / 86400000 + 2440587.5
+
+
+jdToTime : JD -> Time
+jdToTime jd =
+    (jd - 2440587.5) * 86400000
