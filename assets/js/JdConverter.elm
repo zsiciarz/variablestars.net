6e24e65327a3924e17684c74bc668865e6c5aa1b
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-10-10 21:26:22 +0200

    Store both date and current JD in the model.
    Current JD is updated every second.

--- assets/js/JdConverter.elm
+++ assets/js/JdConverter.elm
@@ -5,19 +5,30 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Signal
 import String
+import Time exposing (every, second)
 
 import Astronomy exposing (JD, CustomDate, timeToJd, jdToTime, dateToJd, dateFromJd)
 import Utils exposing (formatJD)
 
 
 main =
-    Signal.map (view actions.address) (Signal.foldp update initModel actions.signal)
+    Signal.map
+        (view actions.address)
+        (Signal.foldp update initModel
+            (Signal.merge tick actions.signal))
 
 
-type alias Model = CustomDate
+tick : Signal Action
+tick = Signal.map (Tick << timeToJd) (every second)
+
+type alias Model =
+    { date : CustomDate
+    , currentJD : JD
+    }
 
 
-initModel = { year = 0,  month = 0, day = 0, hour = 0, minute = 0, second = 0 }
+initDate = { year = 0,  month = 0, day = 0, hour = 0, minute = 0, second = 0 }
+initModel = { date = initDate, currentJD = 0 }
 
 
 type Action
@@ -29,6 +40,7 @@ type Action
     | SetMinute String
     | SetSecond String
     | SetJD String
+    | Tick JD
 
 
 actions : Signal.Mailbox Action
@@ -37,28 +49,32 @@ actions = Signal.mailbox NoOp
 
 update : Action -> Model -> Model
 update action model =
+    let
+        d = model.date
+    in
     case action of
         SetYear value -> case String.toInt value of
-            Ok value -> { model | year <- value }
+            Ok value -> { model | date <- { d | year <- value } }
             Err _ -> model
         SetMonth value -> case String.toInt value of
-            Ok value -> { model | month <- value }
+            Ok value -> { model | date <- { d | month <- value } }
             Err _ -> model
         SetDay value -> case String.toInt value of
-            Ok value -> { model | day <- value }
+            Ok value -> { model | date <- { d | day <- value } }
             Err _ -> model
         SetHour value -> case String.toInt value of
-            Ok value -> { model | hour <- value }
+            Ok value -> { model | date <- { d | hour <- value } }
             Err _ -> model
         SetMinute value -> case String.toInt value of
-            Ok value -> { model | minute <- value }
+            Ok value -> { model | date <- { d | minute <- value } }
             Err _ -> model
         SetSecond value -> case String.toInt value of
-            Ok value -> { model | second <- value }
+            Ok value -> { model | date <- { d | second <- value } }
             Err _ -> model
         SetJD value -> case String.toFloat value of
-            Ok value -> dateFromJd value
+            Ok value -> { model | date <- dateFromJd value }
             Err _ -> model
+        Tick jd -> { model | currentJD <- jd }
 
 
 calendarInput : Signal.Address Action -> Int -> (String -> Action) -> Html
@@ -78,18 +94,18 @@ view address model =
         [ text (toString model)
         , label [] [text "Date and time (UTC)"]
         , div [ class "form-group row" ]
-            [ div [ class "col-xs-2" ] [calendarInput address model.year SetYear]
-            , div [ class "col-xs-2" ] [calendarInput address model.month SetMonth]
-            , div [ class "col-xs-2" ] [calendarInput address model.day SetDay]
-            , div [ class "col-xs-2" ] [calendarInput address model.hour SetHour]
-            , div [ class "col-xs-2" ] [calendarInput address model.minute SetMinute]
-            , div [ class "col-xs-2" ] [calendarInput address model.second SetSecond]
+            [ div [ class "col-xs-2" ] [calendarInput address model.date.year SetYear]
+            , div [ class "col-xs-2" ] [calendarInput address model.date.month SetMonth]
+            , div [ class "col-xs-2" ] [calendarInput address model.date.day SetDay]
+            , div [ class "col-xs-2" ] [calendarInput address model.date.hour SetHour]
+            , div [ class "col-xs-2" ] [calendarInput address model.date.minute SetMinute]
+            , div [ class "col-xs-2" ] [calendarInput address model.date.second SetSecond]
             ],
         div [class "form-group" ]
             [ label [] [text "JD"]
             , input
                 [ class "form-control"
-                , value (formatJD 4 <| dateToJd model)
+                , value (formatJD 4 <| dateToJd model.date)
                 , on "input" targetValue (\str -> Signal.message address (SetJD str))
                 ]
                 []
