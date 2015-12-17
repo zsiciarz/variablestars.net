1c84604b28ffcbfc3b32d401a29e87927aba282e
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-12-17 18:00:10 +0100

    Add migration for Observation model options change.

---
+++ observations/migrations/0002_auto_20151217_1759.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('observations', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='observation',
+            options={'verbose_name': 'Observation', 'ordering': ['-jd'], 'verbose_name_plural': 'Observations'},
+        ),
+    ]
