ae4c08643a7f9af9bc6fb5da80278fab5306133c
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-04-12 17:28:56 +0200

    Force session authentication.

--- variablestars/settings.py
+++ variablestars/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'linaro_django_pagination.middleware.PaginationMiddleware',
