b6bc3b6ddfd8b2aa75da2a1666655a12de66fa72
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-05-03 10:47:03 +0200

    Add upgrade_pip deployment task.

--- fabfile.py
+++ fabfile.py
@@ -37,6 +37,13 @@ def git_pull():
 
 @task
 @roles("web")
+def upgrade_pip():
+    with prepare_project():
+        run("pip install pip setuptools wheel --upgrade")
+
+
+@task
+@roles("web")
 def build_assets():
     local("make production_assets")
 
