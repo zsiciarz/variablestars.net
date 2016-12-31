c89d2817cc122f81a85b2353b86fc5cc69a478d3
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-12-31 19:25:00 +0100

    Manually add debug toolbar to URLconf.

--- variablestars/urls.py
+++ variablestars/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
@@ -15,3 +16,9 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^404/', TemplateView.as_view(template_name='404.html')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
